5f3c47b186703643507b77ff46db1927db5cecea
Guruchandran <44353683+guruchandranb@users.noreply.github.com>
2021-03-28 21:15:29 +0530

    Add files via upload

---
+++ map/map.js
@@ -0,0 +1,37 @@
+import { LightningElement, api, wire  } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+// Set Delivery object fields
+const NAME_FIELD = 'Customer__c.Name';
+const LOCATION_LATITUDE_FIELD = 'Customer__c.Location__Latitude__s';
+const LOCATION_LONGITUDE_FIELD = 'Customer__c.Location__Longitude__s';
+const deliveryFields = [
+	NAME_FIELD,
+	LOCATION_LATITUDE_FIELD,
+	LOCATION_LONGITUDE_FIELD
+];
+
+export default class CustomerMap extends LightningElement {
+    @api recordId;
+  name;
+  mapMarkers = [];
+  @wire(getRecord, { recordId: '$recordId', fields: deliveryFields })
+  loadDelivery({ error, data }) {
+    if (error) {
+      // TODO: handle error
+    } else if (data) {
+      // Get Delivery data
+      this.name =  getFieldValue(data, NAME_FIELD);
+      const Latitude = getFieldValue(data, LOCATION_LATITUDE_FIELD);
+      const Longitude = getFieldValue(data, LOCATION_LONGITUDE_FIELD);
+      // Transform delivery data into map markers
+      this.mapMarkers = [{
+        location: { Latitude, Longitude },
+        title: this.name,
+        description: `Coords: ${Latitude}, ${Longitude}`
+      }];
+    }
+  }
+  get cardTitle() {
+    return (this.name) ? `${this.name}'s location` : 'Delivery location';
+  }
+}
