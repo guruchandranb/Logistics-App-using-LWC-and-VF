2904adbca6f8125726c8c175c3aebd1bef61b92c
Guruchandran <44353683+guruchandranb@users.noreply.github.com>
2021-03-28 22:41:27 +0530

    Update customer.js

--- Customer/customer.js
+++ Customer/customer.js
@@ -1,17 +1,18 @@
 import { LightningElement, api } from 'lwc';
 
 import CUSTOMER from '@salesforce/schema/Customer__c';
+
 import NAME_FIELD from '@salesforce/schema/Customer__c.Name__c';
-import PRODUCT from '@salesforce/schema/Customer__c.Product__c';
-import ADDRESS from '@salesforce/schema/Customer__c.Address__c';
-import STATUS from '@salesforce/schema/Customer__c.Status__c';
+import PHONE from '@salesforce/schema/Customer__c.Phone__c';
 import EMAIL from '@salesforce/schema/Customer__c.Email__c';
+import PICKUP_ADDRESS from '@salesforce/schema/Customer__c.Pickup_Address__c';
+import DELIVERY_ADDRESS from '@salesforce/schema/Customer__c.Delivery_Address__c';
 
 
 export default class Customer extends LightningElement {
 
     accountObject = CUSTOMER;
-    myFields = [NAME_FIELD, PRODUCT, ADDRESS, STATUS,EMAIL];
+    myFields = [NAME_FIELD, PHONE, EMAIL, PICKUP_ADDRESS, DELIVERY_ADDRESS];
 
     handleSubmit(event) {
             console.log('Record ID: ' + event.detail.id);
