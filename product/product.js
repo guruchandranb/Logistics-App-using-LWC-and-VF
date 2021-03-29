8b253deaa0e0bb0142e91809993a8dc9a5f530d5
Guruchandran <44353683+guruchandranb@users.noreply.github.com>
2021-03-29 12:27:36 +0530

    Update product.js

--- product/product.js
+++ product/product.js
@@ -2,10 +2,9 @@ import { LightningElement  } from 'lwc';
 import NAME_FIELD from '@salesforce/schema/Product2__c.Name';
 import ProductCode_FIELD from '@salesforce/schema/Product2__c.ProductCode__c';
 import PRICE__C_FIELD from '@salesforce/schema/Product2__c.Price__c';
-import QUANTITY__C_FIELD from '@salesforce/schema/Product2__c.Quantity__c';
-import TOTAL_PRICE__C_FIELD from '@salesforce/schema/Product2__c.Total_Price__c';
 import DRIVER_NAME__C_FIELD from '@salesforce/schema/Product2__c.Driver_Name__c';
 import DRIVER_PHONE_NUMBER__C_FIELD from '@salesforce/schema/Product2__C.Driver_Phone_Number__c';
+import DELIVERY_ADDRESS__C_FIELD from '@salesforce/schema/Product2__c.Delivery_Address__C';
 
 import PRODUCT2_OBJECT from '@salesforce/schema/Product2';
 
@@ -13,9 +12,7 @@ export default class CustomerPoduct extends LightningElement {
     
  
     objectName = PRODUCT2_OBJECT;
-    
-
-    objectFields = [NAME_FIELD,ProductCode_FIELD,PRICE__C_FIELD,QUANTITY__C_FIELD,TOTAL_PRICE__C_FIELD,DRIVER_NAME__C_FIELD,DRIVER_PHONE_NUMBER__C_FIELD];
+     objectFields = [NAME_FIELD,ProductCode_FIELD,PRICE__C_FIELD,DRIVER_NAME__C_FIELD,DRIVER_PHONE_NUMBER__C_FIELD,DELIVERY_ADDRESS__C_FIELD];
 
     handleLoad(event) {
        console.log(NAME_FIELD);
@@ -25,22 +22,36 @@ export default class CustomerPoduct extends LightningElement {
    }
 
    handleCancel(event) {
+    console.log(ProductCode_FIELD);
+    console.log(PRODUCT2_OBJECT);
        console.log(event.type);
        console.log(JSON.stringify(event.detail));
    }
 
    handleSubmit(event) {
+    console.log(PRICE__C_FIELD);
+    console.log(PRODUCT2_OBJECT);
        console.log(event.type);
        console.log(JSON.stringify(event.detail));
    }
 
     handleSuccess(event) {
+        console.log(DRIVER_NAME__C_FIELD);
+    console.log(PRODUCT2_OBJECT);
         console.log(event.type);
         console.log(JSON.stringify(event.detail));
     }
   
     handleError(event) {
+        console.log(DRIVER_PHONE_NUMBER__C_FIELD);
+    console.log(PRODUCT2_OBJECT);
        console.log(event.type);
        console.log(JSON.stringify(event.detail));
    }
+   handleError(event) {
+    console.log(DELIVERY_ADDRESS__C_FIELD);
+console.log(PRODUCT2_OBJECT);
+   console.log(event.type);
+   console.log(JSON.stringify(event.detail));
+}
 }
